a6c90ee8db63f24f017336bf1aea15563679065e
Hamed Momeni <hamed.ma7@gmail.com>
2019-06-17 09:47:42 +0430

    fixed test

--- schedule_test.go
+++ schedule_test.go
@@ -1,35 +1,39 @@
 package main
 
 import (
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 )
 
-func TestScheduleUnmarshal(t *testing.T) {
-	yamlStr := `
-every: "2:30"
+var yamlStr = `
+every: 2:30
 weekdays:
-	- wed
-	- mon
-	- tue
+ - wed
+ - mon
+ - tue
 monthdays:
-	- 13
+ - 13
 at:
-	- 3:00
-	- 5:13
-	- 15:34
+ - 3:00
+ - 5:13
+ - 15:34
 except:
-	every: "3:45"
-	weekdays:
-	- wed
-	- tue
-	monthdays:
-	- 13
-	at:
-	- 5
+ every: "3:45"
+ weekdays:
+  - wed
+  - tue
+ monthdays:
+  - 13
+ at:
+  - 5
 `
+
+func TestScheduleUnmarshal(t *testing.T) {
+
 	var schedule Schedule
 	err := yaml.Unmarshal([]byte(yamlStr), &schedule)
 
@@ -42,5 +46,17 @@ except:
 
 	if every != 2*3600+30*60 || err != nil {
 		t.Error(errors.Wrap(err, "wrong value for `every`"))
+	}
+
+	weekdays := schedule.Weekdays()
+
+	if !reflect.DeepEqual(weekdays, []time.Weekday{time.Wednesday, time.Monday, time.Tuesday}) {
+		t.Error("wrong value for `weekdays`")
+	}
+
+	monthdays := schedule.monthdays
+
+	if !reflect.DeepEqual(monthdays, []int{13}) {
+		t.Error("wrong value for `monthdays`")
 	}
 }
