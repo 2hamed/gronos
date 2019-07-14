9aeac3a271d593c70351d02b28e13f96cedb0348
Hamed Momeni <hamed.ma7@gmail.com>
2019-07-14 10:39:06 +0430

    moved all scheduler files into a separate package and added a http router

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/2hamed/goronos
 go 1.12
 
 require (
+	github.com/gorilla/mux v1.7.3
 	github.com/pkg/errors v0.8.1
 	gopkg.in/yaml.v2 v2.2.2
 )
--- goronos.go
+++ goronos.go
@@ -2,30 +2,43 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"time"
+
+	"github.com/2hamed/goronos/scheduler"
+	"github.com/gorilla/mux"
 )
 
-var tasks Tasks
+var tasks scheduler.Tasks
 
 func main() {
 	path := os.Args[1]
 
-	tasks, err := LoadTasksFromDir(path)
+	var err error
+
+	tasks, err = scheduler.LoadTasksFromDir(path)
 	if err != nil {
 		panic(err)
 	}
 
 	ticker := time.Tick(5 * time.Second)
-	forever := make(chan struct{})
 
 	go looper(tasks, ticker)
 
-	fmt.Println("Goronos is now active...")
-	<-forever
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", homeHandler)
+	http.Handle("/", r)
+
+	err = http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		panic(err)
+	}
 }
 
-func looper(tasks Tasks, ticker <-chan time.Time) {
+func looper(tasks scheduler.Tasks, ticker <-chan time.Time) {
 	for {
 		t := <-ticker
 		for _, task := range tasks {
@@ -34,4 +47,15 @@ func looper(tasks Tasks, ticker <-chan time.Time) {
 			}
 		}
 	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+	output := []byte("")
+
+	for _, v := range tasks {
+		output = append(output, []byte(fmt.Sprintf("%v\n", v))...)
+	}
+
+	w.Write(output)
 }
