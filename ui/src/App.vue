c2fc5695f274b5b652d6ab6d4142f285e9fc4d05
mostafa.hosseini <mostafa.hosseini@snapp.cab>
2019-08-05 14:59:18 +0430

    Initial commit

---
+++ ui/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    getTasks(){
+      axios.get('http//localhost:8080/tasks').then((res)=>{
+        
+      });
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
