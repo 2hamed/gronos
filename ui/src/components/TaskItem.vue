eacfbc326d0d2c88ffa1f7e55000ea5532c2ed10
mostafa.hosseini <afraprg@gmail.com>
2019-08-10 15:45:27 +0430

    :construction_worker: :construction_worker:

--- ui/src/components/TaskItem.vue
+++ ui/src/components/TaskItem.vue
@@ -2,17 +2,36 @@
   <div>
     <h2>{{task['name']}}</h2>
     <ul>
-      <li v-for="command in task.command">{{command}}</li>
+      <li v-for="command in task['command']" :key="command">{{command}}</li>
     </ul>
+    <button @click="toggleStatus">{{this.taskStatusContrary}} Task</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "TaskItem",
-  props: ["task"],
+  props: ["task", "taskItemStatus", "index"],
   mounted() {
+    // eslint-disable-next-line
     console.log(this.task);
+  },
+  methods: {
+    toggleStatus() {
+      this.$http
+        .get(
+          `/tasks/${this.task.name}/${this.taskStatusContrary.toLowerCase()}`
+        )
+        .then(() => {
+          alert(`Task ${this.taskStatusContrary}d`);
+          this.$emit('listUpdated', this.task);
+        });
+    }
+  },
+  computed: {
+    taskStatusContrary() {
+      return this.taskItemStatus ? "Enable" : "Disable";
+    }
   }
 };
 </script>
--- ui/src/components/Tasks.vue
+++ ui/src/components/Tasks.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <div v-for="task in tasks">
-      <TaskItem :task="task"></TaskItem>
+    <button
+      @click="toggleFilterTasks"
+    >{{displayDisableTasks ? 'Filter By Disabled Tasks' : 'Filter By Enabled Tasks'}}</button>
+    <h2>{{displayDisableTasks ? 'Disable' : 'Enable'}} List</h2>
+    <div v-for="(task, index) in tasks" :key="index">
+      <TaskItem
+        :task="task"
+        :taskItemStatus="displayDisableTasks"
+        @listUpdated="listUpdated"
+        :taskIndex="index"
+      ></TaskItem>
     </div>
+    <div v-show="tasks.length == 0">There are no items to display</div>
   </div>
 </template>
 
@@ -14,17 +24,31 @@ export default {
   data() {
     return {
       loading: false,
-      tasks: []
+      tasks: [],
+      displayDisableTasks: false
     };
   },
   mounted() {
     this.getTasks();
   },
+  computed: {
+    requestUrl() {
+      return this.displayDisableTasks ? "/tasks/disabled" : "/tasks";
+    }
+  },
   methods: {
+    toggleFilterTasks() {
+      this.displayDisableTasks = !this.displayDisableTasks;
+      this.getTasks();
+      return this.displayDisableTasks;
+    },
+    listUpdated() {
+      this.getTasks();
+    },
     getTasks() {
       this.loading = true;
       this.$http
-        .get("/tasks")
+        .get(this.requestUrl)
         .then(res => {
           this.tasks = res.data.data;
         })
